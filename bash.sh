b49ac4f9fc156e796e0b51cda0fbd9a2b17c4cb4
sonht1109 <sonht1109@gmail.com>
2023-07-27 14:01:47 +0700

    update

--- bash.sh
+++ bash.sh
@@ -1,2 +1,4 @@
 sudo curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
+
+sudo apt-get update && sudo apt-get -y install postgresql postgresql-contrib
